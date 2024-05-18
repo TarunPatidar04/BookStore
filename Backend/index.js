d8fbf48003e2517c89b0da16303de077935d20fd
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-05-19 00:10:14 +0530

    connected to mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+const URI = process.env.MONGOBD_URL;
+
+// connect to mongodb server
+try {
+  mongoose
+    .connect(URI)
+    .then(() => {
+      console.log("Connected to mongodb server");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+} catch (error) {}
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
