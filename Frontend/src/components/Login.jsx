ecbf527f1f92348a25773a7d48aa5a8422e22328
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-05-18 20:05:36 +0530

    complete a login page design

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Login</h3>
+          <div className="mt-4 space-y-2">
+            <span>Email</span> <br />
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-80 py-1 px-3 border outline-none rounded-md"
+            />
+          </div>
+          <br />
+          <div className="-mt-2 space-y-2">
+            <span>password</span> <br />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="w-80 py-1 px-3 border outline-none rounded-md"
+            />
+          </div>
+          <div className="flex  justify-between mt-4">
+            <button className="bg-pink-500 text-white px-3 py-1 hover:bg-pink-700 duration-200 rounded-md">
+              Login
+            </button>
+            <p>
+              Not registered ?{" "}
+              <span className="underline text-blue-500 cursor-pointer">
+                Signup
+              </span>{" "}
+            </p>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+};
+
+export default Login;
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Login from "./Login";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -147,9 +148,15 @@ const Navbar = () => {
               </label>
             </div>
             <div className="">
-              <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer">
+              <a
+                className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer"
+                onClick={() =>
+                  document.getElementById("my_modal_3").showModal()
+                }
+              >
                 Login
               </a>
+              <Login />
             </div>
           </div>
         </div>
